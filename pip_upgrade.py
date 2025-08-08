c76807bd8ffbbf9b157ea98c0a16bafdc57fcd25
XJJ76 <117890771+XJJ76@users.noreply.github.com>
2025-08-08 11:30:26 +0800

    Update pip_upgrade.py

--- pip_upgrade.py
+++ pip_upgrade.py
@@ -1,34 +1,39 @@
-import os
+import os,sys
 installed_packages_info = os.popen(f"pip list").readlines()
 old_packages = {}
 for i in installed_packages_info[2:]:
     x = i[:-1].split(' ')
     l = [j for j in x if j != '']
     old_packages[l[0]] = l[1]
+
 outdate_info = os.popen( f'python -m pip list -o')
 outdate_list = outdate_info.readlines()
-update_packages = {}
 
+update_packages = {}
 for i in outdate_list[2:]:
     x = i[:-1].split(' ')
     l = [j for j in x if j != '']
     update_packages[l[0]] = l[2]
 
+if "PySimpleGUI" in update_packages:
+    update_packages.pop("PySimpleGUI")
+
+
+
 for i in update_packages.keys():
     if i == 'pip':
         os.system(f"python -m pip install pip --upgrade --no-cache-dir")
     else:
-        os.system(f'pip install {i}=={update_packages[i]} --no-cache-dir ')
+        os.system(f'python -m pip install {i}=={update_packages[i]} --no-cache-dir ')
 
 #Solve conflict
 print('\nNow solve conflict!!\n')
 conflict1 = os.popen('pip check').readlines()
 if 'No broken requirements' in conflict1[0]:
     print('Congratulation! No conflict found!')
-    exit
+    sys.exit(0)
 conflict_p = []
 comp = ['>','>=','==','<','<=','~=']
-
 def get_first_comp(s):
     l = []
     for i in comp:
@@ -58,7 +63,7 @@ conflict2 = os.popen('pip check').readlines()
 
 if 'No broken requirements' in conflict2[0]:
     print('Congratulation! No conflict found!')
-    exit
+    sys.exit(0)
 conflict_rep = []
 for i in update_packages.keys():
     for j in conflict2:
